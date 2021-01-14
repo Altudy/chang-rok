871da96ae45638f059d2ccb9fb3a4b825ba92619
Han Chang Rok <gksckdfhr99@naver.com>
2021-01-14 22:08:23 +0900

    update Spring folder

---
+++ Back-end/Spring/src/main/java/com/rok/project/service/posts/PostsService.java
@@ -0,0 +1,36 @@
+package com.rok.project.service.posts;
+
+import com.rok.project.domain.posts.Posts;
+import com.rok.project.domain.posts.PostsRepository;
+import com.rok.project.web.dto.PostsResponseDto;
+import com.rok.project.web.dto.PostsSaveRequestDto;
+import com.rok.project.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        return new PostsResponseDto(entity);
+    }
+}
