c0a8f970234518eba970284f8985486abd0ed363
Han Chang Rok <gksckdfhr99@naver.com>
2021-01-17 17:58:13 +0900

    second commit

--- Back-end/Spring/src/main/java/com/rok/project/service/posts/PostsService.java
+++ Back-end/Spring/src/main/java/com/rok/project/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.rok.project.service.posts;
 
 import com.rok.project.domain.posts.Posts;
 import com.rok.project.domain.posts.PostsRepository;
+import com.rok.project.web.dto.PostsListResponseDto;
 import com.rok.project.web.dto.PostsResponseDto;
 import com.rok.project.web.dto.PostsSaveRequestDto;
 import com.rok.project.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,4 +38,15 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return repository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = repository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        repository.delete(posts);
+    }
 }
